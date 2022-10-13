4466ae98b0756d3b77637bec3d34833523d54562
Piotr Filipiak <kiddhkane@gmail.com>
2022-10-13 23:54:41 +0200

    Finished the example app.

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+
 const generateJoke = () => {
-  return "I don't trust stairs. Haha!";
+  // return "I don't trust stairs. Haha!";
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  axios.get("https://icanhazdadjoke.com", config).then((res) => {
+    document.querySelector("#advice").innerText = res.data.joke;
+  });
 };
 
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import brain from "./assets/brain.jpg";
 const brainImg = document.querySelector("#brainImg");
 brainImg.src = brain;
 
-console.log(generateJoke());
+const jokeBtn = document.querySelector("#adviceBtn");
+jokeBtn.addEventListener("click", generateJoke);
+
+// console.log(generateJoke());
+generateJoke();
